ccceba3a8cbcc85ff87355a2b040ad5aaae52e6a
lucasmarinari <lucasmarinari7@gmail.com>
2025-05-27 19:15:35 -0300

    style(navbar): add styling to navbar

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,18 +1,50 @@
+import { useState } from 'react';
 import './navbar.style.css';
+import { Moon, Sun } from 'lucide-react';
 
 export default function Navbar() {
+   const [open, setOpen] = useState(false);
+   const [darkMode, setDarkMode] = useState(false);
+
+   const toggleDarkMode = () => {
+      document.body.classList.toggle('dark');
+      setDarkMode(!darkMode);
+   };
+
    return (
-      <header className='navbar'>
-         <div className='navbar-container'>
-            <div className='logo'>Lucas Tobias</div>
-            <nav className='nav-link'>
-               <a href='home'>Início</a> 
-               <a href='about'>Sobre</a> 
-               <a href='projects'>Projetos</a> 
-               <a href='abilities'>Habilidades</a> 
-               <a href='contact'>Contato</a> 
+      <>
+         <div className={`{sidebard ${open ? 'open' : ''}`}>
+            <div className='theme-toggle' onClick={toggleDarkMode}>
+               {darkMode ? <Sun className='icon-sun' size={30} /> : <Moon className='icon-moon' size={30} />}
+            </div>
+            <nav className='nav-links'>
+               <div className='nav-item'>
+                  <span className='number'>01</span>
+                  <span className='label'>Início</span>
+               </div>
+               <div className='nav-item'>
+                  <span className='number'>02</span>
+                  <span className='label'>Sobre</span>
+               </div>
+               <div className='nav-item'>
+                  <span className='number'>03</span>
+                  <span className='label'>Projetos</span>
+               </div>
+               <div className='nav-item'>
+                  <span className='number'>04</span>
+                  <span className='label'>Habilidades</span>
+               </div>
+               <div className='nav-item'>
+                  <span className='number'>05</span>
+                  <span className='label'>Contato</span>
+               </div>
             </nav>
          </div>
-      </header>
-   );
+
+         <button className='hamburger' onClick={() => setOpen(!open)}>
+            <div className='bar'></div>
+            <div className='bar'></div>
+         </button>
+      </>
+  );
 }
