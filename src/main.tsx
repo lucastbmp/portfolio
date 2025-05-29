0653bbe90e008a7db2d786d1bb938026cdd827e5
lucasmarinari <lucasmarinari7@gmail.com>
2025-05-29 19:18:25 -0300

    style navbar, feat home

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navbar from './components/Navbar/navbar.tsx'
+import Home from './pages/home/home.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <Navbar />
+      <Home />
   </StrictMode>,
 )
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 export default function Home() {
    return(
-      <section id='home' className='home-section'>
-            <h1>Home</h1>
-      </section>
+      <main className='home-container'>
+         <section id='home' className='section-home'>
+            <h1>Olá, eu sou Lucas</h1>
+            <p>Bem-vindo ao meu portfólio.</p>
+         </section>
+      </main>
    );
 }
